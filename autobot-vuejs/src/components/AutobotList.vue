2d45097d50f2264fbd037e2a7849dbc8c6a78d90
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-08-23 00:40:51 +0100

    feat: added simple sample code for the

---
+++ autobot-vuejs/src/components/AutobotList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </h1>
+    </div>
+</template>
+
+<script>
+import api from '../api/axios';
+
+export default {
+    data() {
+
+    },
+    created() {
+        this.fetchAutobots();
+        this.$socket.on('Autobots count', (count) => {
+            console.log('Autobot count:' ${count})
+        });
+    },
+    methods: {
+        async fetchAutobots() {
+            try {
+                const response = await api.get('/autobots');
+                this.autobots = response.data;
+                
+            } catch (error) {
+                console.error('Error occured trying to fetch autobots:',error.message);
+            }
+        }
+    },
+}
+</script>
