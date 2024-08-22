5a6b0736c8200c39d1261a77b0295e2d3385eeeb
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-08-22 01:59:37 +0100

    feat: created a new api.js for routes

---
+++ autobot-nodejs/routes/api.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import Autobot from '../models/Autobot.js';
+import Post from '../models/Post.js';
+import Comment from '../models/Comment.js';
+
+const router = express.Router();
+
+// Rate Limiting Middleware
+import expressRateLimit from 'express-rate-limit';
+const apiLimiter = expressRateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 5,
+  message: "Too many requests, please try again later.",
+});
+
+// Get Autobots
+router.get('/autobots', apiLimiter, async (req, res) => {
+  const autobots = await Autobot.findAll({ limit: 10 });
+  res.json(autobots);
+});
+
+// Get an Autobot's Posts
+router.get('/autobot/:id/posts', apiLimiter, async (req, res) => {
+  const posts = await Post.findAll({ where: { userId: req.params.id }, limit: 10 });
+  res.json(posts);
+});
+
+// Get Post Comments
+router.get('/post/:id/comments', apiLimiter, async (req, res) => {
+  const comments = await Comment.findAll({ where: { postId: req.params.id }, limit: 10 });
+  res.json(comments);
+});
+
+export default router;
