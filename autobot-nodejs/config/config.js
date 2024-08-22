1471bbc9ab070687ad1e94bb7aa1c984f05921b5
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-08-22 12:56:11 +0100

    feat: added config.js for db

---
+++ autobot-nodejs/config/config.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'test_database',
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'production_database',
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+  },
+};
