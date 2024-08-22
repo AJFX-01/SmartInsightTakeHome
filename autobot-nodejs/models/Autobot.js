abeb15734cbe8b02f423592b155952a7d761b201
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2024-08-22 01:00:21 +0100

    feat: define the model for the autobots

---
+++ autobot-nodejs/models/Autobot.js
@@ -0,0 +1,28 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database';
+
+const Autobot = sequelize.define('Autobot', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: DataTypes.STRING,
+  username: DataTypes.STRING,
+  email: DataTypes.STRING,
+  phone: DataTypes.STRING,
+  website: DataTypes.STRING,
+  company_name: DataTypes.STRING,
+  company_catchPhrase: DataTypes.STRING,
+  company_bs: DataTypes.STRING,
+  address_street: DataTypes.STRING,
+  address_suite: DataTypes.STRING,
+  address_city: DataTypes.STRING,
+  address_zipcode: DataTypes.STRING,
+  geo_lat: DataTypes.STRING,
+  geo_lng: DataTypes.STRING,
+}, {
+  timestamps: false,
+});
+
+export default Autobot;
